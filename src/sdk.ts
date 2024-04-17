6dbd0f96fd2a74efc65c4be0a28c5f2b3f6e3443
Liudmyla Ilchenko <116093817+liudmyla-ilchenko@users.noreply.github.com>
2024-04-17 16:30:24 +0300

    project setup + sdk

---
+++ src/sdk.ts
@@ -0,0 +1,57 @@
+import { faker } from '@faker-js/faker';
+import { EventEmitter } from 'events';
+
+const MAX_INTERVAL = 10_000;
+
+const users = [
+  'Alice',
+  'Bob',
+  'Charlie',
+  'David',
+  // 'Eve',
+  // 'Frank',
+  // 'Grace',
+  // 'Heidi',
+  // 'Ivan',
+  // 'Judy',
+];
+
+class SDK extends EventEmitter {
+  constructor() {
+    super();
+
+    this.run();
+    this.message();
+  }
+
+  private run() {
+    setTimeout(() => {
+      this.emit('typing', {
+        user: this.getRandomUser(),
+      });
+
+      this.run();
+    }, this.getRandomInterval());
+  }
+
+  private message() {
+    setTimeout(() => {
+      this.emit('message', {
+        user: this.getRandomUser(),
+        message: faker.lorem.sentence({ min: 3, max: 10 }),
+      });
+
+      this.message();
+    }, this.getRandomInterval());
+  }
+
+  private getRandomInterval() {
+    return Math.floor(Math.random() * MAX_INTERVAL);
+  }
+
+  private getRandomUser() {
+    return users[Math.floor(Math.random() * users.length)];
+  }
+}
+
+export default new SDK();
